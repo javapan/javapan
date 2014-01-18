7b978b19a43e174e3bdceaac88a1a24f58a97cdc
javapan <user@example.fake>
2014-01-17 16:24:27 -0800

    fixed tests
    fixed tests in example proj. removed all assembly. added services
    assembly

--- example/maven_test.bat
+++ example/maven_test.bat
@@ -1,8 +1,5 @@
 cd ../javapan && ^
 mvn clean install && ^
 cd ../example && ^
-mvn clean install && ^
-cd target && ^
-java -Dexample.isMavenBuild=true -jar example-0.0.2-SNAPSHOT-jar-with-dependencies.jar && ^
-cd ..
+mvn clean install
 
